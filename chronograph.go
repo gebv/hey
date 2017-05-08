b324162e015f97e6bbe64082e51c5e5bdd15c888
zhuharev <kirill@zhuharev.ru>
2017-05-08 05:07:13 +0300

    remove uuid dependency

--- chronograph.go
+++ chronograph.go
@@ -1,22 +1,29 @@
 package hey
 
-import (
-	"time"
-
-	uuid "github.com/satori/go.uuid"
-)
+import "time"
 
 type Chronograph interface {
 	// двигаться по lastts
 	RecentActivityByLastTS(threadID string, lastts time.Time) ([]EventObserver, error)
 
-	// двигаться по limit,offset что предлагает tnt
-	RecentActivity(threadID string, limit, offset int) ([]EventObserver, error)
+	// подписка конкретного юзера на трэд
+	Observe(userID, threadID string) error
 
-	NewEvent(
-		threadID,
-		creatorID string,
-		eventID uuid.UUID,
+	ThreadObservers(threadID string) ([]User, error)
+
+	// RecentActivityByLastTS возвращает события позже lastts
+	RecentActivityByLastTS(threadID string, limit, lastts time.Time) ([]Event, error)
+	// двигаться по limit,offset что предлагает tnt
+	RecentActivity(threadID string, limit, offset int) ([]Event, error)
+
+	// events
+	// 1. Достаём всех подписчиков трэда.
+	// 2. Вставляем в Timeline для всех подписчиков этот eventID
+	NewEvent(threadID string, eventID string) error
+
+	NewEventWithData(
+		threadID string,
+		eventID string,
 		dataType DataType,
 		data interface{},
 	) error
