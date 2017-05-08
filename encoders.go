28e3f226f2ee5c0d8aa103739db739ebd36339ea
zhuharev <kirill@zhuharev.ru>
2017-05-08 05:03:18 +0300

    create encoders/decoders for all structs

---
+++ encoders.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"log"
+	"reflect"
+
+	"gopkg.in/vmihailenco/msgpack.v2"
+)
+
+func init() {
+	msgpack.Register(
+		reflect.TypeOf(Thread{}),
+		encodeThread,
+		decodeThread,
+	)
+	msgpack.Register(
+		reflect.TypeOf(Event{}),
+		encodeEvent,
+		decodeEvent,
+	)
+	msgpack.Register(
+		reflect.TypeOf(Threadline{}),
+		encodeThreadline,
+		decodeThreadline,
+	)
+	msgpack.Register(
+		reflect.TypeOf(Observer{}),
+		encodeObserver,
+		decodeObserver,
+	)
+	msgpack.Register(
+		reflect.TypeOf(User{}),
+		encodeUser,
+		decodeUser,
+	)
+
+}
+
+func encodeThread(e *msgpack.Encoder, v reflect.Value) (err error) {
+	m := v.Interface().(Thread)
+
+	if err = e.EncodeSliceLen(3); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.ThreadID); err != nil {
+		return
+	}
+	if err = e.Encode(m.DataType); err != nil {
+		return
+	}
+	if err = e.Encode(m.Data); err != nil {
+		return
+	}
+	return
+}
+
+func decodeThread(d *msgpack.Decoder, v reflect.Value) (err error) {
+	var l int
+
+	m := v.Addr().Interface().(*Thread)
+	if l, err = d.DecodeSliceLen(); err != nil {
+		return
+	}
+
+	if l != 3 {
+		return ErrMsgPackConflictFields
+	}
+
+	if m.ThreadID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if err = d.Decode(&m.DataType); err != nil {
+		return
+	}
+
+	if m.Data, err = FactoryDataObj(m.DataType); err != nil {
+		if err = d.Skip(); err != nil {
+			return
+		}
+		log.Printf("hey: not supported data type DataType(%d)", m.DataType)
+		return ErrNotRegDataType
+	} else if err = d.Decode(&m.Data); err != nil {
+		return
+	}
+
+	return
+}
+
+func encodeEvent(e *msgpack.Encoder, v reflect.Value) (err error) {
+	m := v.Interface().(Event)
+
+	if err = e.EncodeSliceLen(6); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.EventID); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.ThreadID); err != nil {
+		return
+	}
+	if err = e.Encode(m.DataType); err != nil {
+		return
+	}
+	if err = e.Encode(m.Data); err != nil {
+		return
+	}
+	if err = e.EncodeTime(m.CreatedAt); err != nil {
+		return
+	}
+	if err = e.EncodeTime(m.UpdatedAt); err != nil {
+		return
+	}
+	return
+}
+
+func decodeEvent(d *msgpack.Decoder, v reflect.Value) (err error) {
+	var l int
+
+	m := v.Addr().Interface().(*Event)
+	if l, err = d.DecodeSliceLen(); err != nil {
+		return
+	}
+
+	if l != 6 {
+		return ErrMsgPackConflictFields
+	}
+
+	if m.EventID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if m.ThreadID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if err = d.Decode(&m.DataType); err != nil {
+		return
+	}
+
+	if m.Data, err = FactoryDataObj(m.DataType); err != nil {
+		if err = d.Skip(); err != nil {
+			return
+		}
+		log.Printf("hey: not supported data type DataType(%d)", m.DataType)
+		return ErrNotRegDataType
+	} else if err = d.Decode(&m.Data); err != nil {
+		return
+	}
+
+	if m.CreatedAt, err = d.DecodeTime(); err != nil {
+		return
+	}
+	if m.UpdatedAt, err = d.DecodeTime(); err != nil {
+		return
+	}
+
+	return
+}
+
+func encodeThreadline(e *msgpack.Encoder, v reflect.Value) (err error) {
+	m := v.Interface().(Threadline)
+
+	if err = e.EncodeSliceLen(3); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.EventID); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.ThreadID); err != nil {
+		return
+	}
+	if err = e.EncodeTime(m.UpdatedAt); err != nil {
+		return
+	}
+	return
+}
+
+func decodeThreadline(d *msgpack.Decoder, v reflect.Value) (err error) {
+	var l int
+
+	m := v.Addr().Interface().(*Threadline)
+	if l, err = d.DecodeSliceLen(); err != nil {
+		return
+	}
+
+	if l != 3 {
+		return ErrMsgPackConflictFields
+	}
+
+	if m.EventID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if m.ThreadID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if m.UpdatedAt, err = d.DecodeTime(); err != nil {
+		return
+	}
+
+	return
+}
+
+func encodeObserver(e *msgpack.Encoder, v reflect.Value) (err error) {
+	m := v.Interface().(Observer)
+
+	if err = e.EncodeSliceLen(5); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.UserID); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.ThreadID); err != nil {
+		return
+	}
+	if err = e.EncodeTime(m.LastTimeStamp); err != nil {
+		return
+	}
+	if err = e.Encode(m.DataType); err != nil {
+		return
+	}
+	if err = e.Encode(m.Data); err != nil {
+		return
+	}
+	return
+}
+
+func decodeObserver(d *msgpack.Decoder, v reflect.Value) (err error) {
+	var l int
+
+	m := v.Addr().Interface().(*Observer)
+	if l, err = d.DecodeSliceLen(); err != nil {
+		return
+	}
+
+	if l != 5 {
+		return ErrMsgPackConflictFields
+	}
+
+	if m.UserID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if m.ThreadID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if m.LastTimeStamp, err = d.DecodeTime(); err != nil {
+		return
+	}
+	if err = d.Decode(&m.DataType); err != nil {
+		return
+	}
+
+	if m.Data, err = FactoryDataObj(m.DataType); err != nil {
+		if err = d.Skip(); err != nil {
+			return
+		}
+		log.Printf("hey: not supported data type DataType(%d)", m.DataType)
+		return ErrNotRegDataType
+	} else if err = d.Decode(&m.Data); err != nil {
+		return
+	}
+
+	return
+}
+
+func encodeUser(e *msgpack.Encoder, v reflect.Value) (err error) {
+	m := v.Interface().(User)
+
+	if err = e.EncodeSliceLen(3); err != nil {
+		return
+	}
+	if err = e.EncodeString(m.UserID); err != nil {
+		return
+	}
+	if err = e.Encode(m.DataType); err != nil {
+		return
+	}
+	if err = e.Encode(m.Data); err != nil {
+		return
+	}
+	return
+}
+
+func decodeUser(d *msgpack.Decoder, v reflect.Value) (err error) {
+	var l int
+
+	m := v.Addr().Interface().(*User)
+	if l, err = d.DecodeSliceLen(); err != nil {
+		return
+	}
+
+	if l != 3 {
+		return ErrMsgPackConflictFields
+	}
+
+	if m.UserID, err = d.DecodeString(); err != nil {
+		return
+	}
+	if err = d.Decode(&m.DataType); err != nil {
+		return
+	}
+
+	if m.Data, err = FactoryDataObj(m.DataType); err != nil {
+		if err = d.Skip(); err != nil {
+			return
+		}
+		log.Printf("hey: not supported data type DataType(%d)", m.DataType)
+		return ErrNotRegDataType
+	} else if err = d.Decode(&m.Data); err != nil {
+		return
+	}
+
+	return
+}
--- models.go
+++ models.go
@@ -2,12 +2,7 @@ package hey
 
 import (
 	"errors"
-	"log"
-	"reflect"
 	"time"
-
-	uuid "github.com/satori/go.uuid"
-	msgpack "gopkg.in/vmihailenco/msgpack.v2"
 )
 
 var (
@@ -26,117 +21,39 @@ type Thread struct {
 // Events
 
 type Event struct {
-	EventID   uuid.UUID
+	EventID   string
 	ThreadID  string
-	CreatorID string
 	DataType  DataType
 	Data      interface{}
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
+// Threadline - таблица для хранения индекса событий каждой подписки
+// выборка происходит в обратном хронологическом порядке по ThreadID
 type Threadline struct {
-	EventID   uuid.UUID
+	EventID   string
 	ThreadID  string
-	CreatedAt time.Time
-	DataType  DataType
-	Data      interface{}
+	UpdatedAt time.Time
 }
 
+// Observer это таблица для хранения подписок на трэды.
+// используется при создании нового события в трэде,
+// для всех подписчиков этого трэда
+// создаётся запись в таблице Threadline
 type Observer struct {
-	UserID                  string
-	ThreadID                string
-	ParentThreadID          string
-	RelatedThreadlineExists bool
-	LastTimeStamp           time.Time // unix time stamp
-	DataType                DataType
-	Data                    interface{}
+	UserID   string
+	ThreadID string
+	//ParentThreadID          string
+	//RelatedThreadlineExists bool
+	LastTimeStamp time.Time // unix time stamp
+	DataType      DataType
+	Data          interface{}
 }
 
-type EventObserver struct {
-	ObserverID string // related Observer.UserID
-
-	Event      Event
-	Threadline Threadline
-}
-
+// User подписчик, обозреватель,
 type User struct {
 	UserID   string
 	DataType DataType
 	Data     interface{}
-}
-
-// Serializer for thread
-
-func init() {
-	msgpack.Register(
-		reflect.TypeOf(Thread{}),
-		encodeThread,
-		decodeThread,
-	)
-}
-
-func encodeThread(e *msgpack.Encoder, v reflect.Value) error {
-	m := v.Interface().(Thread)
-
-	if err := e.EncodeSliceLen(3); err != nil {
-		return err
-	}
-
-	//1
-	if err := e.EncodeString(m.ThreadID); err != nil {
-		return err
-	}
-	//2
-	if err := e.EncodeUint32(uint32(m.DataType)); err != nil {
-		return err
-	}
-	//3
-	if err := e.Encode(m.Data); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func decodeThread(d *msgpack.Decoder, v reflect.Value) error {
-	var err error
-	var l int
-
-	m := v.Addr().Interface().(*Thread)
-
-	if l, err = d.DecodeSliceLen(); err != nil {
-		return err
-	}
-
-	if l != 3 {
-		return ErrMsgPackConflictFields
-	}
-
-	//1
-	if data, err := d.DecodeString(); err == nil {
-		m.ThreadID = data
-	} else {
-		return err
-	}
-	//2
-	if data, err := d.DecodeUint32(); err == nil {
-		m.DataType = DataType(data)
-	} else {
-		return err
-	}
-
-	//5
-	m.Data, err = FactoryDataObj(m.DataType)
-	if err == nil {
-		if err = d.Decode(&m.Data); err != nil {
-			return err
-		}
-	} else {
-		d.Skip()
-		log.Printf("hey: not supported data type DataType(%d)", m.DataType)
-		return ErrNotRegDataType
-	}
-
-	return nil
 }
