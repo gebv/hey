d0fc7fd842ec500b9616adb76554a84e012e3462
zhuharev <kirill@zhuharev.ru>
2017-05-09 04:34:57 +0300

    change chronograph from interface to struct

--- chronograph.go
+++ chronograph.go
@@ -1,39 +1,20 @@
 package hey
 
-import "time"
+// Chronograph represent main api
+type Chronograph struct {
+	Manager
+}
 
-// Chronograph represents storage methods
-type Chronograph interface {
-	// threads
-	NewThread(threadID string) error
+// New create chronograph with default manager (tarantool)
+func New() (*Chronograph, error) {
+	manager, err := NewTarantoolManager()
+	if err != nil {
+		return nil, err
+	}
+	return NewWithManager(manager)
+}
 
-	NewThreadWithData(threadID string, dataType DataType, data interface{}) error
-
-	// 1. Удаляем все записи из events
-	DeleteThread(threadID) error
-
-	// подписка конкретного юзера на трэд
-	Observe(userID, threadID string) error
-
-	ThreadObservers(threadID string) ([]User, error)
-
-	// RecentActivityByLastTS возвращает события позже lastts
-	RecentActivityByLastTS(threadID string, limit, lastts time.Time) ([]Event, error)
-	// двигаться по limit,offset что предлагает tnt
-	RecentActivity(threadID string, limit, offset int) ([]Event, error)
-
-	// events
-	// 1. Достаём всех подписчиков трэда.
-	// 2. Вставляем в Timeline для всех подписчиков этот eventID
-	NewEvent(threadID string, eventID string) error
-
-	NewEventWithData(
-		threadID string,
-		eventID string,
-		dataType DataType,
-		data interface{},
-	) error
-
-	UpdateEvent(ev *Event) error
-	DeleteEvent(eventID string) error
+// NewWithManager create chronograph with passed store
+func NewWithManager(manager Manager) (*Chronograph, error) {
+	return &Chronograph{Manager: manager}, nil
 }
