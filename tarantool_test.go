99c0a6bdf99a688241273fc081683015a4a7d61d
zhuharev <kirill@zhuharev.ru>
2017-05-14 23:08:20 +0300

    add tests

--- tarantool_test.go
+++ tarantool_test.go
@@ -135,34 +135,81 @@ func TestRecentActivityThreadline(t *testing.T) {
 	err = chrono.NewThread(thread)
 	assert.NoError(t, err)
 
+	thread2 := &Thread{}
+	err = chrono.NewThread(thread2)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, thread2.ThreadID)
+
 	user1 := &User{
 		UserID: "user2",
 	}
 	err = chrono.NewUser(user1)
 	assert.NoError(t, err)
 
+	user2 := &User{
+		UserID: "user3",
+	}
+	err = chrono.NewUser(user2)
+	assert.NoError(t, err)
+
+	user3 := &User{
+		UserID: "user4",
+	}
+	err = chrono.NewUser(user3)
+	assert.NoError(t, err)
+
 	err = chrono.Observe(user1.UserID, thread.ThreadID)
+	assert.NoError(t, err)
+	err = chrono.Observe(user3.UserID, thread.ThreadID)
+	assert.NoError(t, err)
+
+	observers, err := chrono.Observers(thread.ThreadID, 0, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(observers))
+	assert.Equal(t, user1.UserID, observers[0].UserID)
+	assert.Equal(t, user3.UserID, observers[1].UserID)
+
+	err = chrono.Ignore(user1.UserID, thread.ThreadID)
+	assert.NoError(t, err)
+
+	observers, err = chrono.Observers(thread.ThreadID, 0, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(observers))
+	assert.Equal(t, user3.UserID, observers[0].UserID)
+
+	err = chrono.Observe(user3.UserID, thread2.ThreadID)
+	assert.NoError(t, err)
+
+	observes, err := chrono.Observes(user3.UserID, 0, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(observes))
+	assert.Equal(t, thread.ThreadID, observes[0].ThreadID)
+	assert.Equal(t, thread2.ThreadID, observes[1].ThreadID)
+
+	// невозможно проверить
+	now := time.Now()
+	err = chrono.MarkAsDelivered(user3.UserID, thread.ThreadID, now)
 	assert.NoError(t, err)
 
 	lastTs := time.Now()
 
 	event1 := &Event{
-		EventID:   "ev5",
+		EventID:   "ev55",
 		ThreadID:  thread.ThreadID,
 		CreatedAt: time.Now(),
 	}
 	event2 := &Event{
-		EventID:   "ev6",
+		EventID:   "ev66",
 		ThreadID:  thread.ThreadID,
 		CreatedAt: time.Now(),
 	}
 	event3 := &Event{
-		EventID:   "ev7",
+		EventID:   "ev77",
 		ThreadID:  thread.ThreadID,
 		CreatedAt: time.Now(),
 	}
 	event4 := &Event{
-		EventID:   "ev8",
+		EventID:   "ev88",
 		ThreadID:  thread.ThreadID,
 		CreatedAt: time.Now(),
 	}
@@ -175,8 +222,8 @@ func TestRecentActivityThreadline(t *testing.T) {
 	events, err := chrono.RecentActivityByLastTS(user1.UserID, thread.ThreadID, 3, lastTs)
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(events))
-	assert.Equal(t, "ev8", events[0].EventID)
-	assert.Equal(t, "ev7", events[1].EventID)
-	assert.Equal(t, "ev6", events[2].EventID)
+	assert.Equal(t, "ev88", events[0].EventID)
+	assert.Equal(t, "ev77", events[1].EventID)
+	assert.Equal(t, "ev66", events[2].EventID)
 
 }
